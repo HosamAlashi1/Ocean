06e9b0d6ed063bd40e3346aa5ae76dc23ee46f94
HosamAlashi1 <as3adel3shi@gmail.com>
2025-04-21 13:04:57 +0300

    cors problem

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/',[HomeController::class,'index']);
+Route::get('/home',[HomeController::class,'index']);
 
 Route::post('/join-our-weekly',[SubscribeController::class,'add']);
 
