3533f6f5cdef11289a9bfd2af1795c757dee215f
HosamAlashi1 <as3adel3shi@gmail.com>
2025-05-19 17:37:53 +0300

    add seo settings

--- routes/web.php
+++ routes/web.php
@@ -108,3 +108,12 @@ Route::group(
 
 });
 
+Route::get('/deploy', function (\Illuminate\Http\Request $request) {
+    if ($request->query('token') !== 'h5sam1234') {
+        abort(403, 'Unauthorized.');
+    }
+
+    $output = shell_exec('cd ' . base_path() . ' && git pull origin main && php artisan migrate --force && php artisan config:cache && php artisan route:cache');
+
+    return response("<pre>$output</pre>", 200)->header('Content-Type', 'text/plain');
+});
